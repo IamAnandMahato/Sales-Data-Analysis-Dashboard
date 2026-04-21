ada4f84c566da2ec546f2e84837f2078fa7e4020
IamAnandMahato <129567237+IamAnandMahato@users.noreply.github.com>
2026-04-21 11:31:41 +0530

    Update db.py

--- src/db.py
+++ src/db.py
@@ -1,1 +1,13 @@
+import mysql.connector
+import pandas as pd
+from config.config import DB_CONFIG
 
+def get_connection():
+    return mysql.connector.connect(**DB_CONFIG)
+
+def fetch_data():
+    conn = get_connection()
+    query = "SELECT * FROM sales"
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
